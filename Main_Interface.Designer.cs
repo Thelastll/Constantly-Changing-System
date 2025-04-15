f55cf44cd6f13e5af92d8252116788a9e6edda54
The Last Left <prostodima54769@gmail.com>
2025-04-15 20:32:35 +0300

    create main interface layout

--- Main_Interface.Designer.cs
+++ Main_Interface.Designer.cs
@@ -28,22 +28,307 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main_Interface));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.stop_input = new System.Windows.Forms.NumericUpDown();
+            this.display = new System.Windows.Forms.Button();
+            this.edit = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.describe = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.iter_input = new System.Windows.Forms.NumericUpDown();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.stop = new System.Windows.Forms.Button();
+            this.run = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.output = new System.Windows.Forms.RichTextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.stop_input)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iter_input)).BeginInit();
+            this.tableLayoutPanel3.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.output, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 72F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 28F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1262, 673);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.stop_input, 0, 6);
+            this.tableLayoutPanel2.Controls.Add(this.display, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.edit, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.label2, 0, 5);
+            this.tableLayoutPanel2.Controls.Add(this.describe, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label1, 0, 3);
+            this.tableLayoutPanel2.Controls.Add(this.iter_input, 0, 4);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(1009, 0);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 7;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.44444F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5.555555F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.44444F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5.555555F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(253, 484);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // stop_input
+            // 
+            this.stop_input.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.stop_input.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.stop_input.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.stop_input.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.stop_input.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.stop_input.Location = new System.Drawing.Point(0, 452);
+            this.stop_input.Margin = new System.Windows.Forms.Padding(0);
+            this.stop_input.Name = "stop_input";
+            this.stop_input.Size = new System.Drawing.Size(253, 18);
+            this.stop_input.TabIndex = 10;
+            this.stop_input.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // display
+            // 
+            this.display.BackColor = System.Drawing.Color.Honeydew;
+            this.display.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.display.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.display.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.display.FlatAppearance.BorderSize = 2;
+            this.display.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DarkSlateGray;
+            this.display.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightCyan;
+            this.display.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.display.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.display.Location = new System.Drawing.Point(12, 204);
+            this.display.Margin = new System.Windows.Forms.Padding(12);
+            this.display.Name = "display";
+            this.display.Size = new System.Drawing.Size(229, 72);
+            this.display.TabIndex = 8;
+            this.display.Text = "Отображение объектов";
+            this.display.UseVisualStyleBackColor = false;
+            // 
+            // edit
+            // 
+            this.edit.BackColor = System.Drawing.Color.Honeydew;
+            this.edit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.edit.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.edit.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.edit.FlatAppearance.BorderSize = 2;
+            this.edit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DarkSlateGray;
+            this.edit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightCyan;
+            this.edit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.edit.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.edit.Location = new System.Drawing.Point(12, 108);
+            this.edit.Margin = new System.Windows.Forms.Padding(12);
+            this.edit.Name = "edit";
+            this.edit.Size = new System.Drawing.Size(229, 72);
+            this.edit.TabIndex = 7;
+            this.edit.Text = "Редактировать объекты";
+            this.edit.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Honeydew;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(0, 383);
+            this.label2.Margin = new System.Windows.Forms.Padding(0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(253, 69);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Число итераций перед приостановкой";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // describe
+            // 
+            this.describe.BackColor = System.Drawing.Color.Honeydew;
+            this.describe.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.describe.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.describe.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.describe.FlatAppearance.BorderSize = 2;
+            this.describe.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DarkSlateGray;
+            this.describe.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightCyan;
+            this.describe.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.describe.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.describe.Location = new System.Drawing.Point(12, 12);
+            this.describe.Margin = new System.Windows.Forms.Padding(12);
+            this.describe.Name = "describe";
+            this.describe.Size = new System.Drawing.Size(229, 72);
+            this.describe.TabIndex = 0;
+            this.describe.Text = "Описать параметры";
+            this.describe.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Honeydew;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(0, 288);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(253, 69);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Количество итераций";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // iter_input
+            // 
+            this.iter_input.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.iter_input.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.iter_input.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.iter_input.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.iter_input.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.iter_input.Location = new System.Drawing.Point(0, 357);
+            this.iter_input.Margin = new System.Windows.Forms.Padding(0);
+            this.iter_input.Name = "iter_input";
+            this.iter_input.Size = new System.Drawing.Size(253, 18);
+            this.iter_input.TabIndex = 9;
+            this.iter_input.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.stop, 0, 1);
+            this.tableLayoutPanel3.Controls.Add(this.run, 0, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(1009, 484);
+            this.tableLayoutPanel3.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(253, 189);
+            this.tableLayoutPanel3.TabIndex = 1;
+            // 
+            // stop
+            // 
+            this.stop.BackColor = System.Drawing.Color.Honeydew;
+            this.stop.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.stop.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.stop.Enabled = false;
+            this.stop.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.stop.FlatAppearance.BorderSize = 2;
+            this.stop.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DarkSlateGray;
+            this.stop.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightCyan;
+            this.stop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.stop.Font = new System.Drawing.Font("Arial Rounded MT Bold", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stop.ForeColor = System.Drawing.Color.Black;
+            this.stop.Location = new System.Drawing.Point(12, 106);
+            this.stop.Margin = new System.Windows.Forms.Padding(12);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(229, 71);
+            this.stop.TabIndex = 2;
+            this.stop.Text = "Остановить";
+            this.stop.UseVisualStyleBackColor = false;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // run
+            // 
+            this.run.BackColor = System.Drawing.Color.Honeydew;
+            this.run.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.run.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.run.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.run.FlatAppearance.BorderSize = 2;
+            this.run.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DarkSlateGray;
+            this.run.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightCyan;
+            this.run.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.run.Font = new System.Drawing.Font("Arial Rounded MT Bold", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.run.Location = new System.Drawing.Point(12, 12);
+            this.run.Margin = new System.Windows.Forms.Padding(12);
+            this.run.Name = "run";
+            this.run.Size = new System.Drawing.Size(229, 70);
+            this.run.TabIndex = 1;
+            this.run.Text = "Запустить";
+            this.run.UseVisualStyleBackColor = false;
+            this.run.Click += new System.EventHandler(this.run_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.Honeydew;
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(30, 30);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(30, 30, 30, 15);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(949, 439);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // output
+            // 
+            this.output.BackColor = System.Drawing.Color.Honeydew;
+            this.output.Cursor = System.Windows.Forms.Cursors.Default;
+            this.output.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.output.Font = new System.Drawing.Font("Verdana", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.output.Location = new System.Drawing.Point(30, 499);
+            this.output.Margin = new System.Windows.Forms.Padding(30, 15, 30, 30);
+            this.output.Name = "output";
+            this.output.ReadOnly = true;
+            this.output.Size = new System.Drawing.Size(949, 144);
+            this.output.TabIndex = 3;
+            this.output.Text = "";
             // 
             // Main_Interface
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1302, 713);
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Main_Interface";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Constantly Changing System";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.stop_input)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iter_input)).EndInit();
+            this.tableLayoutPanel3.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button describe;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.Button run;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button display;
+        private System.Windows.Forms.Button edit;
+        private System.Windows.Forms.NumericUpDown iter_input;
+        private System.Windows.Forms.NumericUpDown stop_input;
+        private System.Windows.Forms.RichTextBox output;
     }
 }
--- Main_Interface.cs
+++ Main_Interface.cs
@@ -19,6 +19,20 @@ namespace ConstantlyChangingSystem
 
         private void Form1_Load(object sender, EventArgs e)
         {
+        
+        }
+
+        private void run_Click(object sender, EventArgs e)
+        {
+            stop.Enabled = true;
+            run.Text = "Продолжить";
+
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            stop.Enabled = false;
+            run.Text = "Запустить";
         }
     }
 }
